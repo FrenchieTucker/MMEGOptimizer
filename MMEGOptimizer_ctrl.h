0fe000515631dc51c6470a025938675f9b47fbef
Florian Leméasle <florian@lemeasle.fr>
2019-04-11 19:04:29 +0200

    Ajout des fichiers principaux pour pouvoir tester la fonctionnalité d'import

---
+++ MMEGOptimizer_ctrl.h
@@ -0,0 +1,40 @@
+/*
+This program helps the user to optimize a glyph's build for an
+invocation in the game : Might&Magic : Elemental Guardians
+Copyright (C) 2019  Florian LEMEASLE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef MMEGOPTIMIZER_WDG_H
+#define MMEGOPTIMIZER_WDG_H
+
+#include <QMainWindow>
+
+#include "ui_MMEGOptimizer.h"
+
+class MMEGOptimizer_ctrl
+{
+public:
+    explicit MMEGOptimizer_ctrl();
+    ~MMEGOptimizer_ctrl() = default;
+
+    void show();
+
+private:
+    Ui::MMEGOptimizer_wdg ui;
+    QMainWindow m_wdg;
+};
+
+#endif // MMEGOPTIMIZER_WDG_H
---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*
+This program helps the user to optimize a glyph's build for an
+invocation in the game : Might&Magic : Elemental Guardians
+Copyright (C) 2019  Florian LEMEASLE
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "MMEGOptimizer_ctrl.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MMEGOptimizer_ctrl w;
+    w.show();
+
+    return a.exec();
+}
